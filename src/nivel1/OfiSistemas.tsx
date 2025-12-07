96126eb6e526c1c11ff798323fada08a289efbb9
Especial2x4 <especial2x4@gmail.com>
2025-12-07 18:44:35 -0300

    poniendo esqueletos en el suelo

---
+++ src/nivel1/OfiSistemas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="OfiSistemas" tilewidth="16" tileheight="16" tilecount="999" columns="37">
+ <image source="ofi_sistemas_tile_Image_a92y74a92y74a92y.png" width="592" height="432"/>
+</tileset>
---
+++ src/nivel1/OficinaSistemas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="OficinaSistemas" tilewidth="16" tileheight="16" tilecount="3996" columns="74">
+ <image source="ofi_sistemas_tile_Image_a92y74a92y74a92y.png" width="1184" height="864"/>
+</tileset>
